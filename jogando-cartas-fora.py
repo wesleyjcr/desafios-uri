c4ffe9572e13f08d0aa5a6b982e853005764681b
wesleyjcr <wesleyjcr@gmail.com>
2021-10-12 11:27:59 -0300

    Jogando cartas fora

---
+++ jogando-cartas-fora.py
@@ -0,0 +1,30 @@
+#https://www.urionlinejudge.com.br/judge/pt/problems/view/1110
+
+
+entrada = int(input())
+descarte = []
+
+while entrada != 0:
+    lista = list(range(1, entrada + 1))
+    
+    while len(lista)>= 2: 
+        descarte.append(lista[0])
+        del lista[0]
+
+        lista.append(lista[0])
+        del lista[0]
+
+    sobra = lista[0]
+    print_result = ', '.join(str(e) for e in descarte)
+    print(f'Discarded cards: {print_result}')
+    print(f'Remaining card: {sobra}')    
+
+    lista = []
+    descarte = []
+    sobra = None
+    entrada = int(input())
+
+
+    
+
+
